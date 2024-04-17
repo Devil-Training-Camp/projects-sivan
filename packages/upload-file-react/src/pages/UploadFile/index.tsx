3e70a03d3378b3965f8b55683ef4fc6cb01f78a7
Sivan <1758299424@qq.com>
2024-04-18 00:28:06 +0800

    feat: splitFile

--- packages/upload-file-react/src/pages/UploadFile/index.tsx
+++ packages/upload-file-react/src/pages/UploadFile/index.tsx
@@ -1,27 +1,56 @@
+import { useState, useMemo } from "react";
 import { Upload, Button } from "antd";
 import type { UploadChangeParam } from "antd/es/upload";
+import prettsize from "prettysize";
+import { splitFile } from "../../utils/file";
 import styles from "./index.module.scss";
 
 const UploadFile = () => {
-  const onChange = (info: UploadChangeParam) => {
-    console.log(info);
+  const [file, setFile] = useState<File | null>(null);
+
+  const beforeUpload = (file: File) => {
+    setFile(file);
+    // 阻止默认上传
+    return false;
   };
+
+  const fileSize = useMemo(() => prettsize(file?.size), [file]);
+
+  const onChange = (info: UploadChangeParam) => {};
+
+  const onUpload = () => {
+    if (!file) return;
+    const fileChunkList = splitFile(file);
+    console.log(fileChunkList);
+  };
+
+  const onDelete = () => setFile(null);
 
   return (
     <div className={styles.container}>
       <Upload
         showUploadList={false}
+        beforeUpload={beforeUpload}
         onChange={onChange}
         className={styles.upload}
       >
         选择文件
       </Upload>
       <div className={styles.operation}>
-        <Button type="primary" className={styles.optItem}>
+        <Button type="primary" className={styles.optItem} onClick={onUpload}>
           上传
         </Button>
         <Button className={styles.optItem}>暂停</Button>
+        <Button
+          type="primary"
+          danger
+          className={styles.optItem}
+          onClick={onDelete}
+        >
+          删除
+        </Button>
       </div>
+      {!file ? null : <div>文件大小为{fileSize}</div>}
     </div>
   );
 };
---
+++ packages/upload-file-react/src/utils/file.ts
@@ -0,0 +1,17 @@
+import { CHUNK_SIZE } from "../const";
+
+export interface FilePiece {
+  chunk: Blob;
+  size: number;
+}
+
+export const splitFile = (file: File, chunkSize = CHUNK_SIZE) => {
+  const fileChunkList: FilePiece[] = [];
+  let cur = 0;
+  while (cur < file.size) {
+    const piece = file.slice(cur, cur + chunkSize);
+    fileChunkList.push({ chunk: piece, size: piece.size });
+    cur += chunkSize;
+  }
+  return fileChunkList;
+};
